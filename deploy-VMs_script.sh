71c1e254685aa310ec1ad2f667f93f5189b40394
Rahul-1611 <rahuldeshmukh1611@gmail.com>
2025-05-11 12:11:46 -0400

    Saving deployment progress

--- deploy-VMs_script.sh
+++ deploy-VMs_script.sh
@@ -4,6 +4,8 @@
 RESOURCE_GROUP="vmFleetCommander"
 LOCATION="westus"
 BICEP_FILE="main.bicep"
+PARAMETER_FILE="test.parameters.json"
+# .bicepparam file input is not supported in az CLI (only Bicep CLI)
 
 # ==== PARAMETERS ====
 ADMIN_PASSWORD="<yourSecurePassword>" # Or use read -s to securely prompt
@@ -19,11 +21,20 @@ az group create \
   --name $RESOURCE_GROUP \
   --location $LOCATION
 
-# ==== DEPLOY BICEP FILE ====
+# # === VALIDATE BICEP AND PARAM FILES ===
+# az deployment group validate \
+#   --resource-group "$RESOURCE_GROUP" \
+#   --template-file main.bicep \
+#   --parameters @"$PARAMETER_FILE" adminPassword="$ADMIN_PASSWORD"
+
+
+echo "Validated the files"
+
+==== DEPLOY BICEP FILE ====
 az deployment group create \
   --resource-group $RESOURCE_GROUP \
   --template-file $BICEP_FILE \
-  --parameters \
+  --parameters @"$PARAMETER_FILE" \
     adminPassword=$ADMIN_PASSWORD
 
 # ==== DONE ====
